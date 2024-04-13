a020728701ef8d0e386b4ceb0fccaee62555cf5c
Justin Wilford <justin.wilford@codeflip.dev>
2024-04-13 16:11:43 -0500

    Populate Exercise Class

--- Domain/Exercise.cs
+++ Domain/Exercise.cs
@@ -2,5 +2,12 @@ namespace HealthApi;
 
 public class Exercise
 {
-
+    public int ExerciseId { get; set; }
+    public int TotalTime { get; set; }
+    public int Run { get; set; }
+    public int Walk { get; set; }
+    public int Bike { get; set; }
+    public int Hike { get; set; }
+    public int UserId { get; set; }
+    public DateTime ExerciseDate { get; set; }
 }
