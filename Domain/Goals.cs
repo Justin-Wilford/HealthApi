1c7c5450d0c356932659b0e7fadc124fc1631a9e
Justin Wilford <justin.wilford@codeflip.dev>
2024-04-13 18:59:17 -0500

    Populate Goals and HeartRate Class

--- Domain/Goals.cs
+++ Domain/Goals.cs
@@ -2,5 +2,8 @@ namespace HealthApi;
 
 public class Goals
 {
-    
+    public int GoalId { get; set; }
+    public int StepGoal { get; set; }
+    public int WaterIntakeGoal { get; set; }
+    public int UserId { get; set; }
 }
--- Domain/HeartRate.cs
+++ Domain/HeartRate.cs
@@ -2,5 +2,9 @@ namespace HealthApi;
 
 public class HeartRate
 {
-    
+    public int HeartRateId { get; set; }
+    public int AverageHeartRate { get; set; }
+    public int UserId { get; set; }
+    public DateTime HeartRateDate { get; set; }
+
 }
