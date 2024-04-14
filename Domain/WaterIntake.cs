68ca7b52dfe216e95030af52051ea495335d455e
Justin Wilford <justin.wilford@codeflip.dev>
2024-04-13 19:44:18 -0500

    Populate WaterIntake Class

--- Domain/WaterIntake.cs
+++ Domain/WaterIntake.cs
@@ -2,5 +2,8 @@ namespace HealthApi;
 
 public class WaterIntake
 {
-    
+    public int WaterIntakeId { get; set; }
+    public int DailyWaterIntake { get; set; }
+    public int UserId { get; set; }
+    public DateTime WaterIntakeDate { get; set; }
 }
